e9b9d50231251865f34defb9dbba4e27c721a932
bala-amypo <bala@amypo.in>
2025-12-17 06:24:47 +0000

    User commit

--- src/main/java/com/example/demo/Entity/Role.java
+++ src/main/java/com/example/demo/Entity/Role.java
@@ -1,8 +1,13 @@
 import.jakarta.persistence.Entity;
 import.jakarta.persistence.Id;
+import.jakarta.persistence.Column;
 @Entity
 public class Role{
     @Id
-    private
+    private Long id;
+    @Column (unique=true)
+    private string roleName;
+    private string 
+
     
 }
