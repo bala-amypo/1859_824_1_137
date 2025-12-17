d0445d934d6bf2e183d22fc4c3b1c4267ddfab18
bala-amypo <bala@amypo.in>
2025-12-17 08:36:42 +0000

    User commit

--- src/main/java/com/example/demo/Entity/Permission.java
+++ src/main/java/com/example/demo/Entity/Permission.java
@@ -1,44 +1,44 @@
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Column;
-@Entity
-public class Permission{
-    @Id
-    private Long id;
-     @Column (unique=true)
-    private string PermissionKey;
-    private String description;
-    private Boolean active;
-     public Long getid()
-{
-    return id;
-}
-public void setid(Long id)
-{
-    this.id=id;
-} 
-public String getpermissionKey()
-{
-    return PermissionKey;
-}
-public void setpermissionKey(string PermissionKey)
-{
-    this.PermissionKey=PermissionKey;
-}
-public string getdescription()
-{
-    return description;
-}
-public void setdescription(string description )
-{
-    this.description=description;
-}
-public string getactive()
-{
-    return active;
-}
-public void setactive(Boolean active)
-{
-    this.active=active;
-}    
-}
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.Column;
+// @Entity
+// public class Permission{
+//     @Id
+//     private Long id;
+//      @Column (unique=true)
+//     private string PermissionKey;
+//     private String description;
+//     private Boolean active;
+//      public Long getid()
+// {
+//     return id;
+// }
+// public void setid(Long id)
+// {
+//     this.id=id;
+// } 
+// public String getpermissionKey()
+// {
+//     return PermissionKey;
+// }
+// public void setpermissionKey(string PermissionKey)
+// {
+//     this.PermissionKey=PermissionKey;
+// }
+// public string getdescription()
+// {
+//     return description;
+// }
+// public void setdescription(string description )
+// {
+//     this.description=description;
+// }
+// public string getactive()
+// {
+//     return active;
+// }
+// public void setactive(Boolean active)
+// {
+//     this.active=active;
+// }    
+// }
--- src/main/java/com/example/demo/Entity/RolePermission.java
+++ src/main/java/com/example/demo/Entity/RolePermission.java
@@ -1,9 +1,9 @@
-import  jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-@Entity
-public class RolePermission{
-    @Id
-    private Long id;
+// import  jakarta.persistence.Entity;
+// import jakarta.persistence.Id;
+// @Entity
+// public class RolePermission{
+//     @Id
+//     private Long id;
     
     
-}
+// }
