0a06d2ed7208b43c1f92fd0dd31e2d9696eee743
bala-amypo <bala@amypo.in>
2025-12-18 07:53:59 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserAccountRepository.java
+++ src/main/java/com/example/demo/repository/UserAccountRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.entity.UserAccount;
-public interface UserAccountRepo extends JpaRepository<UserAccount,Long>{
-    
+public interface UserAccountRepository extends JpaRepository<UserAccount,Long>{
+
 }
--- src/main/java/serviceimplements/UserAccountserimp.java
+++ src/main/java/serviceimplements/UserAccountserimp.java
@@ -3,14 +3,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.example.demo.Entity.UserAccount;
-import com.example.demo.repository.UserAccountRepo;
+import com.example.demo.repository.UserAccountRepository;
 import com.example.demo.service.UserAccountser;
 
 @Service
 public class UserAccountserimp implements UserAccountser{
     @Autowired
-    UserAccountRepo uar;
-    
+    UserAccountRepository uar;
+    public User
+
     UserAccount createUser(UserAccount user);
     UserAccount updateUser(Long id,UserAccount user);
     UserAccount getUserById(Long id);
