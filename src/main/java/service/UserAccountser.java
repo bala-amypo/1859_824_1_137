8cbfd21de12584219ee5b112a4d6fc800da0ef1d
bala-amypo <bala@amypo.in>
2025-12-18 06:13:59 +0000

    User commit

--- src/main/java/service/UserAccountser.java
+++ src/main/java/service/UserAccountser.java
@@ -1,5 +1,9 @@
+package com.example.demo.service;
+import java.util.List;
+import com.example.demo.entity.UserAccount;
+
 public interface UserAccountser
 {
-    void createUser(User);
+    Accountuser createUser(User);
 
 }
