60b0e255589600a55d621e7020b40ca8507fde7d
bala-amypo <bala@amypo.in>
2025-12-18 03:53:59 +0000

    User commit

--- src/main/java/com/example/demo/Entity/Role.java
+++ src/main/java/com/example/demo/Entity/Role.java
@@ -1,31 +1,31 @@
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.Column;
-// @Entity
-// public class Role{
-//     @Id
-//     private Long id;
-//     @Column (unique=true)
-//     private string roleName;
-//     private string description;
-//     private Boolean active;
-//     public Long getid()
-// {
-//     return id;
-// }
-// public void setid(Long id)
-// {
-//     this.id=id;
-// } 
-// public String getroleName()
-// {
-//     return roleName;
-// }
-// public void setroleName(string roleName)
-// {
-//     this.roleName=roleName;
-// }
-// public string getdescription()
+ import jakarta.persistence.Entity;
+ import jakarta.persistence.Id;
+ import jakarta.persistence.Column;
+ @Entity
+ public class Role{
+    @Id
+    private Long id;
+     @Column (unique=true)
+     private string roleName;
+     private string description;
+     private Boolean active;
+     public Long getid()
+ {
+     return id;
+ }
+ public void setid(Long id)
+ {
+     this.id=id;
+ } 
+ public String getroleName()
+ {
+     return roleName;
+ }
+ public void setroleName(string roleName)
+ {
+     this.roleName=roleName;
+ }
+ public string getdescription()
 // {
 //     return description;
 // }
