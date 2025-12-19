84ca3f8a288bc34e9281107e5188ac1037406b90
bala-amypo <bala@amypo.in>
2025-12-19 05:17:21 +0000

    User commit

--- src/main/java/service/serviceimpl/ UserAccountServiceImpl.java
+++ src/main/java/service/serviceimpl/ UserAccountServiceImpl.java
@@ -51,7 +51,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.emvnntity.UserAccount;
+import com.example.demo.entity.UserAccount;
 import com.example.demo.repository.UserAccountRepository;
 import com.example.demo.service.UserAccountService;
 
