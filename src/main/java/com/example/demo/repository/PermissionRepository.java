37d894be621848178e8ce3674f0b06c3b200c241
bala-amypo <bala@amypo.in>
2025-12-26 15:50:52 +0000

    User commit

--- src/main/java/com/example/demo/repository/PermissionRepository.java
+++ src/main/java/com/example/demo/repository/PermissionRepository.java
@@ -1,9 +1,12 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
+import com.example.demo.entity.Permission;
+import org.springframework.stereotype.Repository;
 
+import java.util.*;
+
+@Repository
 public interface PermissionRepository extends JpaRepository<Permission, Long> {
     Optional<Permission> findByPermissionKey(String permissionKey);
 }
--- src/main/java/com/example/demo/repository/RoleRepository.java
+++ src/main/java/com/example/demo/repository/RoleRepository.java
@@ -1,11 +1,13 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.example.demo.entity.Role;
 
+import java.util.*;
+
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
-
-    boolean existsByRoleName(String roleName);
-
-    Role findByRoleName(String roleName);
+    Optional<Role> findByRoleName(String roleName);
 }
